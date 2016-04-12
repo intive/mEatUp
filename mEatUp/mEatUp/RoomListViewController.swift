ac4091e17aaabfeceb9a530217f25a486072c7e7
SeeNyu <ilsp.lwc.totosamo@gmail.com>
2016-04-12 14:31:54 +0200

    [ADD] Segue between RoomListViewController and RoomViewController (#6)
    * [ADD] Segue between RoomListViewController and RoomViewController
    
    * [REMOVE] Unused method in RoomViewController

--- mEatUp/mEatUp/RoomListViewController.swift
+++ mEatUp/mEatUp/RoomListViewController.swift
@@ -11,3 +11,16 @@ import UIKit
 class RoomListViewController: UIViewController {
 
 }
+
+extension RoomListViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RoomCell", forIndexPath: indexPath)
+        return cell
+    }
+    
+    //function added only to conform UITableViewDataSource protocol
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+}
---
+++ mEatUp/mEatUp/RoomViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RoomViewController.swift
+//  mEatUp
+//
+//  Created by Krzysztof Przybysz on 12/04/16.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import UIKit
+
+class RoomViewController: UIViewController {
+    
+}
