bbc6353903c98e169cdb1b2922af7bb46be3238a
Krzysztof Przybysz <krzysztof.przybysz@blstream.com>
2016-04-12 14:19:19 +0200

    [ADD] Segue between RoomListViewController and RoomViewController

---
+++ mEatUp/mEatUp/RoomViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RoomViewController.swift
+//  mEatUp
+//
+//  Created by Krzysztof Przybysz on 12/04/16.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import UIKit
+
+class RoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
