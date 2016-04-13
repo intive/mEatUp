874ddb468f78797a9ebc32033e346ac51261faa5
Maciej Plewko <maciej.plewko@blstream.com>
2016-04-13 11:06:55 +0200

    [FIX] Facebook login fix

--- mEatUp/mEatUp/LoginViewController.swift
+++ mEatUp/mEatUp/LoginViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
-import FBSDKShareKit
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     
@@ -21,10 +20,11 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
         configureFacebook()
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(true)
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
         if FBSDKAccessToken.currentAccessToken() != nil
         {
+            loginButton.hidden = true
             performSegueWithIdentifier("ShowRoomListSegue", sender: nil)
         }
     }
@@ -34,26 +34,26 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     }
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print("Logged in")
         if error != nil {
         } else {
             FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(small)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+                if error != nil {
+                } else {
+                    let firstName: String? = (result.objectForKey("first_name") as? String)
+                    let lastName: String? = (result.objectForKey("last_name") as? String)
+                    let pictureURL: String? = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)
             
-                let firstName: String? = (result.objectForKey("first_name") as? String)
-                let lastName: String? = (result.objectForKey("last_name") as? String)
-                let pictureURL: String? = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)
-            
-                self.userDefaults.setValue(firstName, forKey: "first_name")
-                self.userDefaults.setValue(lastName, forKey: "last_name")
-                self.userDefaults.setValue(pictureURL, forKey: "picture_url")
-            
-                print("Welcome, \(firstName) \(lastName) pictureURL: \(pictureURL)")
+                    self.userDefaults.setValue(firstName, forKey: "first_name")
+                    self.userDefaults.setValue(lastName, forKey: "last_name")
+                    self.userDefaults.setValue(pictureURL, forKey: "picture_url")
+                }
             }
         }
     }
     
+    //function added only to conform FBSDKLoginButtonDelegate protocol
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-        print("Logged out")
+
     }
     
 }
