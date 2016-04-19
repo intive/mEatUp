78bbb6b3e62327dcc8b851421fcd094b74512e1e
Krzysztof Przybysz <krzysztof.przybysz@blstream.com>
2016-04-19 16:14:07 +0200

    [HOTFIX] ScaleAspectFit for images from RoomViewController and SettlementViewController, RoomParticipantTableViewCell update

--- mEatUp/mEatUp/RoomParticipantTableViewCell.swift
+++ mEatUp/mEatUp/RoomParticipantTableViewCell.swift
@@ -14,28 +14,9 @@ class RoomParticipantTableViewCell: UITableViewCell {
     
     func configureWithRoom(user: User) {
         nameLabel.text = "\(user.name ?? "") \(user.surname ?? "")"
-//        user.photo = "https://scontent.fwaw3-1.fna.fbcdn.net/hphotos-xaf1/v/t1.0-9/374213_283413375043181_689553604_n.jpg?oh=6dd2ac9a55150b4b663fcb398c63f575&oe=57AB61B6"
-        if let photoURL = user.photo {
-            if let checkedUrl = NSURL(string: photoURL) {
-                self.imgView.contentMode = .ScaleAspectFit
-                downloadImage(checkedUrl)
-            }
+        if let URLasString = user.photo, let url = NSURL(string: URLasString) {
+            imgView.setImageFromUrl(url)
         }
-    }
-    
-    private func downloadImage(url: NSURL){
-        getDataFromUrl(url) { (data, response, error)  in
-            dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                guard let data = data where error == nil else { return }
-                self.imgView.image = UIImage(data: data)
-            }
-        }
-    }
-    
-    private func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
-        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
-            completion(data: data, response: response, error: error)
-            }.resume()
     }
 
 }
