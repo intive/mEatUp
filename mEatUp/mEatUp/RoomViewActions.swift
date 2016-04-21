cab728ca119dc16d6775415154d9b01682e8af49
Paweł Knuth <pawel.knuth@blstream.com>
2016-04-21 10:58:33 +0200

    [ADD] Added RoomViewPurpose and RoomViewActions enums, also RoomViewController is configured depending on purpose

---
+++ mEatUp/mEatUp/RoomViewActions.swift
@@ -0,0 +1,13 @@
+//
+//  RoomViewActions.swift
+//  mEatUp
+//
+//  Created by Paweł Knuth on 21.04.2016.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import Foundation
+
+enum RoomViewActions: String {
+    case Delete, Join, Leave
+}
