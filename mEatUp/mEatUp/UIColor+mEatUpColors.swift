c908ff08bd4761280b8db88bbcea93d538cc021f
Maciej Plewko <maciej.plewko@blstream.com>
2016-05-09 16:44:53 +0200

    [ADD] mEatUp colors

---
+++ mEatUp/mEatUp/UIColor+mEatUpColors.swift
@@ -0,0 +1,44 @@
+//
+//  UIColor+mEatUpColors.swift
+//  mEatUp
+//
+//  Created by Maciej Plewko on 09.05.2016.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    // czerwona pomarańcza
+    @nonobjc static let darkPastelRedColor = UIColor(red:0.81, green:0.22, blue:0.13, alpha:1.00)
+    @nonobjc static let redOrangeColor = UIColor(red:1.00, green:0.27, blue:0.17, alpha:1.00)
+    @nonobjc static let bittersweetColor = UIColor(red:0.97, green:0.46, blue:0.35, alpha:1.00)
+    @nonobjc static let monaLisaColor = UIColor(red:0.98, green:0.64, blue:0.56, alpha:1.00)
+    // szary
+    @nonobjc static let riverBedColor = UIColor(red:0.36, green:0.37, blue:0.38, alpha:1.00)
+    @nonobjc static let ravenColor = UIColor(red:0.45, green:0.46, blue:0.47, alpha:1.00)
+    @nonobjc static let aluminiumColor = UIColor(red:0.59, green:0.60, blue:0.60, alpha:1.00)
+    @nonobjc static let silverSandColor = UIColor(red:0.73, green:0.73, blue:0.73, alpha:1.00)
+    @nonobjc static let darkGrayCustomColor = UIColor(red:0.33, green:0.33, blue:0.33, alpha:1.00)
+    @nonobjc static let darkerGrayCustomColor = UIColor(red:0.65, green:0.65, blue:0.65, alpha:1.00)
+    @nonobjc static let gainsboroColor = UIColor(red:0.87, green:0.87, blue:0.87, alpha:1.00)
+    // piaskowy
+    @nonobjc static let tussockColor = UIColor(red:0.80, green:0.62, blue:0.28, alpha:1.00)
+    @nonobjc static let saffronMangoColor = UIColor(red:1.00, green:0.77, blue:0.36, alpha:1.00)
+    @nonobjc static let goldenGlowColor = UIColor(red:1.00, green:0.83, blue:0.51, alpha:1.00)
+    @nonobjc static let navajoWhiteColor = UIColor(red:1.00, green:0.88, blue:0.67, alpha:1.00)
+    // pomarańczowy
+    @nonobjc static let piperColor = UIColor(red:0.79, green:0.38, blue:0.10, alpha:1.00)
+    @nonobjc static let pumpkinColor = UIColor(red:0.99, green:0.47, blue:0.14, alpha:1.00)
+    @nonobjc static let sunshadeColor = UIColor(red:0.99, green:0.60, blue:0.29, alpha:1.00)
+    @nonobjc static let maccaroniAndCheeseColor = UIColor(red:0.99, green:0.73, blue:0.52, alpha:1.00)
+    //bordo
+    @nonobjc static let merlotColor = UIColor(red:0.53, green:0.13, blue:0.16, alpha:1.00)
+    @nonobjc static let brownCustomColor = UIColor(red:0.66, green:0.16, blue:0.20, alpha:1.00)
+    @nonobjc static let chestnutColor = UIColor(red:0.75, green:0.37, blue:0.40, alpha:1.00)
+    @nonobjc static let canCanColor = UIColor(red:0.83, green:0.58, blue:0.60, alpha:1.00)
+    @nonobjc static let pohutukawaColor = UIColor(red:0.37, green:0.12, blue:0.16, alpha:1.00)
+    @nonobjc static let tamarilloColor = UIColor(red:0.46, green:0.15, blue:0.20, alpha:1.00)
+    @nonobjc static let vinRougeColor = UIColor(red:0.60, green:0.36, blue:0.40, alpha:1.00)
+    @nonobjc static let rosyBrownColor = UIColor(red:0.73, green:0.57, blue:0.60, alpha:1.00)
+}
