e94b25241bf32c2e80a8c328a42c861ea2a6fac2
Macple <maciej.plewko@gmail.com>
2016-05-10 10:21:28 +0200

    [ADD] mEatUp colors (#64)
    * [ADD] mEatUp colors
    
    * [MOD] UIColor properties changed into functions

---
+++ mEatUp/mEatUp/UIColor+mEatUpColors.swift
@@ -0,0 +1,43 @@
+//
+//  UIColor+mEatUpColors.swift
+//  mEatUp
+//
+//  Created by Maciej Plewko on 09.05.2016.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    // czerwona pomarańcza
+    class func darkPastelRedColor() -> UIColor { return UIColor(red:0.81, green:0.22, blue:0.13, alpha:1.00) }
+    class func redOrangeColor() -> UIColor { return UIColor(red:1.00, green:0.27, blue:0.17, alpha:1.00) }
+    class func bittersweetColor() -> UIColor { return UIColor(red:0.97, green:0.46, blue:0.35, alpha:1.00) }
+    class func monaLisaColor() -> UIColor { return UIColor(red:0.98, green:0.64, blue:0.56, alpha:1.00) }
+    // szary
+    class func driverBedColor() -> UIColor { return UIColor(red:0.36, green:0.37, blue:0.38, alpha:1.00) }
+    class func ravenColor() -> UIColor { return UIColor(red:0.59, green:0.60, blue:0.60, alpha:1.00) }
+    class func aluminiumColor() -> UIColor { return UIColor(red:0.59, green:0.60, blue:0.60, alpha:1.00) }
+    class func silverSandColor() -> UIColor { return UIColor(red:0.73, green:0.73, blue:0.73, alpha:1.00) }
+    class func darkGrayCustomColor() -> UIColor { return UIColor(red:0.33, green:0.33, blue:0.33, alpha:1.00) }
+    class func darkerGrayCustomColor() -> UIColor { return UIColor(red:0.65, green:0.65, blue:0.65, alpha:1.00) }
+    class func gainsboroColor() -> UIColor { return UIColor(red:0.87, green:0.87, blue:0.87, alpha:1.00) }
+    // piaskowy
+    class func tussockColor() -> UIColor { return UIColor(red:0.80, green:0.62, blue:0.28, alpha:1.00) }
+    class func saffronMangoColor() -> UIColor { return UIColor(red:1.00, green:0.77, blue:0.36, alpha:1.00) }
+    class func goldenGlowColor() -> UIColor { return UIColor(red:1.00, green:0.83, blue:0.51, alpha:1.00) }
+    class func navajoWhiteColor() -> UIColor { return UIColor(red:1.00, green:0.88, blue:0.67, alpha:1.00) }
+    // pomarańczowy
+    class func piperColor() -> UIColor { return UIColor(red:0.79, green:0.38, blue:0.10, alpha:1.00) }
+    class func pumpkinColor() -> UIColor { return UIColor(red:0.99, green:0.47, blue:0.14, alpha:1.00) }
+    class func sunshadeColor() -> UIColor { return UIColor(red:0.99, green:0.60, blue:0.29, alpha:1.00) }
+    class func maccaroniAndCheeseColor() -> UIColor { return UIColor(red:0.99, green:0.73, blue:0.52, alpha:1.00) }
+    //bordo
+    class func merlotColor() -> UIColor { return UIColor(red:0.53, green:0.13, blue:0.16, alpha:1.00) }
+    class func brownCustomColor() -> UIColor { return UIColor(red:0.66, green:0.16, blue:0.20, alpha:1.00) }
+    class func chestnutColor() -> UIColor { return UIColor(red:0.75, green:0.37, blue:0.40, alpha:1.00) }
+    class func canCanColor() -> UIColor { return UIColor(red:0.83, green:0.58, blue:0.60, alpha:1.00) }
+    class func pohutukawaColor() -> UIColor { return UIColor(red:0.37, green:0.12, blue:0.16, alpha:1.00) }
+    class func tamarilloColor() -> UIColor { return UIColor(red:0.46, green:0.15, blue:0.20, alpha:1.00) }
+    class func rosyBrownColor() -> UIColor { return UIColor(red:0.73, green:0.57, blue:0.60, alpha:1.00) }
+}
