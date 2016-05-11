e2bb96c10eb295c4894d2ffbe3cc4988f9224523
Maciej Plewko <maciej.plewko@blstream.com>
2016-05-11 15:18:07 +0200

    [ADD] Colors with places to use

---
+++ mEatUp/mEatUp/UIColor+whereToUseColor.swift
@@ -0,0 +1,52 @@
+//
+//  UIColor+whereToUseColor.swift
+//  mEatUp
+//
+//  Created by Maciej Plewko on 11.05.2016.
+//  Copyright © 2016 BLStream. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func navigationBarBarTintColor() -> UIColor {
+        return UIColor.redOrangeColor()
+    }
+    
+    class func navigationBarTintColor() -> UIColor {
+        return UIColor.whiteColor()
+    }
+    
+    class func navigationBarTitleTextColor() -> UIColor {
+        return UIColor.whiteColor()
+    }
+    
+    class func searchBarTintColor() -> UIColor {
+        return UIColor.redOrangeColor()
+    }
+    
+    class func tabBarBarTintColor() -> UIColor {
+        return UIColor.redOrangeColor()
+    }
+    
+    class func tabBarTintColor() -> UIColor {
+        return whiteColor()
+    }
+    
+    class func loginButtonBackgroundColor() -> UIColor {
+        return whiteColor()
+    }
+    
+    class func loginButtonBorderColor() -> UIColor {
+        return UIColor.redOrangeColor()
+    }
+    
+    class func loginButtonTitleColorNormal() -> UIColor {
+        return UIColor.redOrangeColor()
+    }
+    
+    class func loginButtonTitleColorHighlited() -> UIColor {
+        return UIColor.aluminiumColor()
+    }
+}
